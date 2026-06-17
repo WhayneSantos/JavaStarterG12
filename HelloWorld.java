cda4fff6a465fd848b3e0b7ecbcb07bdfad65355
WhayneSantos <sandwichzack3@gmail.com>
2026-06-17 11:42:51 +0800

    myfirstcommit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("I'm Whayne Zackarry Santos");
+        System.out.println("I am 17 years old");
+    }
+}
