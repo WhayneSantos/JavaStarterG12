d3d35da68c634a7c385e051cc7ac214b37cf4b07
WhayneSantos <sandwichzack3@gmail.com>
2026-06-17 12:50:52 +0800

    seccommit

--- BasicStrucAndSyntax/HelloWorld.java
+++ BasicStrucAndSyntax/HelloWorld.java
@@ -3,6 +3,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("I'm Whayne Zackarry Santos");
+        System.out.println("I am a student at Cielito Senior High School");
+        System.out.println(136635150258L);
         System.out.println("I am 17 years old");
+        System.out.println("addition" );
+        System.out.println(5 + 3);
+        System.out.println("subtraction");
+        System.out.println(10 - 4);
+        System.out.println("multiplication");
+        System.out.println(6 * 7);
+        System.out.println("division");
+        System.out.println(20 / 4);
     }
 }
