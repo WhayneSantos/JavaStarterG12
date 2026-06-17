ae4f0bfbf0a467d7c07f694ad28229d674db8169
WhayneSantos <sandwichzack3@gmail.com>
2026-06-17 13:08:23 +0800

    testcalculator

--- BasicStrucAndSyntax/HelloWorld.java
+++ BasicStrucAndSyntax/HelloWorld.java
@@ -6,13 +6,17 @@ public class HelloWorld {
         System.out.println("I am a student at Cielito Senior High School");
         System.out.println(136635150258L);
         System.out.println("I am 17 years old");
-        System.out.println("addition" );
+        System.out.println("addition" ); //this is addition
+        System.out.print("5 + 3 = ");
         System.out.println(5 + 3);
-        System.out.println("subtraction");
+        System.out.println("subtraction"); //this is subtraction
+        System.out.print("10 - 4 = ");
         System.out.println(10 - 4);
-        System.out.println("multiplication");
+        System.out.println("multiplication"); //this is multiplication
+        System.out.print("6 * 7 = ");
         System.out.println(6 * 7);
-        System.out.println("division");
+        System.out.println("division"); //this is division
+        System.out.print("20 / 4 = ");
         System.out.println(20 / 4);
     }
 }
